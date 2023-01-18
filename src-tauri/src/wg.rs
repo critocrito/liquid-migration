f2eca8e28b9774dc0beabd67b68b2217de83d547
Christo Buschek <christo.buschek@proton.me>
2023-01-18 22:17:54 +0100

    restore wireguard key pair from base64 encoded strings

--- src-tauri/src/wg.rs
+++ src-tauri/src/wg.rs
@@ -1,7 +1,16 @@
 use rand_core::OsRng;
 use serde::Serialize;
 use std::fmt;
+use thiserror::Error;
 use x25519_dalek::{PublicKey, StaticSecret};
+
+#[derive(Debug, Error)]
+pub enum WgError {
+    #[error(transparent)]
+    Decode(#[from] base64::DecodeError),
+    #[error("parsing key from string")]
+    Parse,
+}
 
 #[derive(Serialize)]
 pub struct Wireguard {
@@ -24,6 +33,25 @@ impl Wireguard {
     pub fn public_encoded(&self) -> String {
         base64::encode(self.public.to_bytes())
     }
+
+    pub fn from_encoded(privkey: &str, pubkey: &str) -> Result<Self, WgError> {
+        let privkey_decoded = base64::decode(privkey)?;
+        let pubkey_decoded = base64::decode(pubkey)?;
+
+        let privkey: &[u8; 32] = privkey_decoded
+            .as_slice()
+            .try_into()
+            .map_err(|_| WgError::Parse)?;
+        let pubkey: &[u8; 32] = pubkey_decoded
+            .as_slice()
+            .try_into()
+            .map_err(|_| WgError::Parse)?;
+
+        Ok(Self {
+            secret: StaticSecret::from(privkey.to_owned()),
+            public: PublicKey::from(pubkey.to_owned()),
+        })
+    }
 }
 
 impl fmt::Debug for Wireguard {
@@ -32,5 +60,21 @@ impl fmt::Debug for Wireguard {
             .field("secret", &self.secret_encoded())
             .field("public", &self.public_encoded())
             .finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn restore_keys_from_string() {
+        let privkey = "IB+p0pBDrhJvehQHuIzRu0Go5dzAk2ad+Cx6ayNjCFI=";
+        let pubkey = "Y7rK8JM7kW3eqhOa7Ei5TMwA+yHCYy51g5A1+2HIemw=";
+
+        let wg = Wireguard::from_encoded(&privkey, pubkey).unwrap();
+
+        assert_eq!(wg.secret_encoded(), privkey);
+        assert_eq!(wg.public_encoded(), pubkey);
     }
 }
