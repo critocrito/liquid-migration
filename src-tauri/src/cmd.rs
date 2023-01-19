b2e3a150c4376177f099b76d329cef3a0dd4f59c
Christo Buschek <christo.buschek@proton.me>
2023-01-19 14:34:12 +0100

    restore files to patch from backup before applying the patch

--- src-tauri/src/cmd.rs
+++ src-tauri/src/cmd.rs
@@ -63,10 +63,25 @@ pub(crate) fn sudo_patch_file(
     patch: &str,
     target: &str,
 ) -> Result<String, CmdError> {
+    let mut backup_path = Path::new(target).to_path_buf();
+    backup_path.set_extension(&format!(
+        "{}.orig",
+        backup_path
+            .extension()
+            .unwrap_or_default()
+            .to_string_lossy()
+    ));
+
+    if backup_path.exists() {
+        println!("Backup exists: {}", backup_path.to_string_lossy());
+        sudo_move_file(password, &backup_path.to_string_lossy(), target)?;
+    }
+
     let mut child = Command::new("sudo")
         .arg("-S")
         .arg("-k")
         .arg("patch")
+        .arg("-b")
         .arg(target)
         .arg(patch)
         .stdin(Stdio::piped())
@@ -134,6 +149,38 @@ pub(crate) fn sudo_copy_file(
         .arg("-S")
         .arg("-k")
         .arg("cp")
+        .arg(source)
+        .arg(target)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let child_stdin = child.stdin.as_mut();
+    if let Some(stdin) = child_stdin {
+        stdin.write_all(password.as_bytes())?;
+        stdin.write_all(b"\n")?;
+    }
+
+    let output = child.wait_with_output()?;
+
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        Err(CmdError::Sudo(
+            String::from_utf8_lossy(&output.stderr).to_string(),
+        ))
+    }
+}
+
+pub(crate) fn sudo_move_file(
+    password: &str,
+    source: &str,
+    target: &str,
+) -> Result<String, CmdError> {
+    let mut child = Command::new("sudo")
+        .arg("-S")
+        .arg("-k")
+        .arg("mv")
         .arg(source)
         .arg(target)
         .stdin(Stdio::piped())
